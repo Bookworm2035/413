873c13a03e9af9873e267ec4993c090365c9006f
Bookworm2035 <126933809+Bookworm2035@users.noreply.github.com>
2024-01-17 22:03:26 -0700

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using static System.Runtime.InteropServices.JavaScript.JSType;
+using System.Runtime.Intrinsics.X86;
+using diceapp;
+public class Program
+{//built in class that is automaticaly there hehe
+    private static void Main()
+    {
+        //this class gets the number of rolls and then prints the histogram using an array from second class
+        Console.WriteLine("Welcome to the dice throwing simulator!");
+        Console.Write("How many dice rolls would you like to simulate? ");
+        //get the user imput 
+        int numberOfRolls = int.Parse(Console.ReadLine());
+        //call rollDice class and create a new instance
+        rollDice rd = new rollDice(numberOfRolls);
+        //get values from other class
+        int[] rollTotals = rd.rollTotals;
+
+        //labels for the histogram
+        Console.WriteLine("DICE ROLLING SIMULATION RESULTS");
+        Console.WriteLine($"Each \"*\" represents 1% of the total number of rolls.");
+        Console.WriteLine($"Total number of rolls = {numberOfRolls}.");
+
+        //for loop to cycle through each die value
+        for (int i = 2; i < rollTotals.Length; i++)
+        {
+            //int Percentage =  rollTotals[i] / numberOfRolls * 100;
+            double Percentage = ((double)rollTotals[i] / numberOfRolls) * 100;
+            //Percentage = num of rolls for each number  DIVIDED BY TOTAL ROLLS *100;
+            string result = new string('*', (int)(Percentage));
+            //write out the ***
+            Console.Write(($"{i} : {result} \n"));
+        }
+    }
+}
+
+
+
---
+++ rollDice.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace diceapp
+{
+    //new class that is public to the Program class
+    internal class rollDice
+    {   //public array for roll values
+        public int[] rollTotals;
+        public rollDice( int numberOfRolls)
+        {   //array with 13 slots
+            rollTotals = new int[13];
+            //get random num
+            Random random = new Random();
+            //initialize die
+            int die1 = 0;
+            int die2 = 0;
+            int total = 0;
+            //for each roll generate values
+            for (int i = 0; i < numberOfRolls; i++)
+            {
+                // Generate a random number between 1 and 6 (inclusive)
+                die1 = random.Next(1, 7);
+                die2 = random.Next(1, 7);
+                total = die1 + die2;
+                //store to array
+                rollTotals[total]++;
+            }
+        }
+    }
+}
+
+
